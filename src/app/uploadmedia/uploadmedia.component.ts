1158e8f8fd848be2d3fc4e0694d592fa53bf96e9
JasmineRengaraj <52908128+JasmineRengaraj@users.noreply.github.com>
2021-08-21 14:35:40 +0530

    ui changes

--- src/app/uploadmedia/uploadmedia.component.ts
+++ src/app/uploadmedia/uploadmedia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,Input  } from '@angular/core';
 import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
 import { of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -14,16 +14,24 @@ import { GlobalsService } from '../globals.service';
 })
 export class UploadmediaComponent  {
   public username: string = this.globals.username;
+  title: string = null;
+  desc: string = null;
+  tags: string = null;
+  effect:string=null
  @ViewChild("fileUpload", {static: false}) fileUpload: ElementRef;files  = [];
  // Inject service
  constructor(private uploadService: FileUploadService,private globals: GlobalsService) { }
  ngOnInit(): void {  }
 
  uploadFile(file) {
+
      const formData = new FormData();
      console.log(file.data)
      formData.append('file', file.data);
-     formData.append('title', file.name);
+     formData.append('title',this.title);
+     formData.append('desc', this.desc);
+     formData.append('tags', this.tags);
+     formData.append('effect',this.effect);
      formData.append('userId', '1');
      file.inProgress = true;
      this.uploadService.upload(formData).pipe(
